cfe8acc7eaf67d02d0a534dc65d294c8226707ae
Yazan Othman <yazan.othman@intel.com>
2026-04-01 00:55:40 +0300

    Add installation script for required R packages and create normalization workflow for scRNA-seq analysis

--- install_packages.R
+++ install_packages.R
@@ -1,1 +1,37 @@
-install.packages(c("Seurat", "tidyverse", "patchwork"), repos="https://cloud.r-project.org")
+# Ensure user library exists
+user_lib <- Sys.getenv("R_LIBS_USER")
+dir.create(user_lib, recursive = TRUE, showWarnings = FALSE)
+.libPaths(user_lib)
+
+# Fix download method for Windows
+options(download.file.method = "wininet")
+
+# CRAN packages
+install.packages(c(
+  "Seurat",
+  "tidyverse",
+  "patchwork",
+  "Matrix",
+  "BiocManager",
+  "pheatmap",
+  "ggrepel",
+  "factoextra",
+  "msigdbr",
+  "devtools"
+), repos = "https://cran.rstudio.com/", lib = user_lib)
+
+# Bioconductor packages
+BiocManager::install(c(
+  "SingleR",
+  "celldex",
+  "SingleCellExperiment",
+  "DESeq2",
+  "apeglm",
+  "org.Hs.eg.db",
+  "clusterProfiler",
+  "enrichplot",
+  "GSVA",
+  "dittoSeq",
+  "EnhancedVolcano",
+  "glmGamPoi"
+), ask = FALSE, update = FALSE, lib = user_lib)
